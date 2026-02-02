d1553d0d9b47cb75450feb3c3672f47b3db773f2
woxdog <woxdog@protonmail.com>
2026-02-02 15:04:16 +0800

    Update Admin.vue

--- web/src/views/Admin.vue
+++ web/src/views/Admin.vue
@@ -59,7 +59,7 @@
       </div>
       <div class="admin-content">
         <div v-if="page==='welcome'" class="welcome-page">
-          <h2 class="welcome-title">欢迎您进入 Nav-Item 后台管理系统</h2>
+          <h2 class="welcome-title">欢迎您进入 非凡导航 后台管理系统</h2>
           <div class="welcome-cards">
             <div class="welcome-card">
               <div class="welcome-icon time-icon">
@@ -84,7 +84,7 @@
         <UserManage v-if="page==='user'" />
       </div>
       <footer class="admin-footer">
-        <p class="admin-copyright">Copyright © 2025 Nav-Item | <a href="https://github.com/eooce/Nav-Item" target="_blank" class="footer-link">Powered by eooce</a></p>
+        <p class="admin-copyright">Copyright © 2025-2026 非凡导航 all rights reserved</p>
       </footer>
     </main>
   </div>
@@ -615,4 +615,5 @@ function closeSider() {
 .menu-toggle {
   display: none;
 }
+
 </style> 
