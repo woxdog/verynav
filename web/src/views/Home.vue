dd4c416c4a4a27b8c1725535ac42ae6258be8678
woxdog <woxdog@protonmail.com>
2026-02-02 21:46:32 +0800

    Update Home.vue

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -56,7 +56,14 @@
     
     <footer class="footer">
       <div class="footer-content">
-        <p class="copyright">Copyright © 2025-2026 非凡导航 all rights reserved</p>
+        <p class="copyright">Copyright © 2025-2026 非凡导航 all rights reserved </p>
+        <button @click="showFriendLinks = true" class="friend-link-btn">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+            <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+            <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+          </svg>
+          友情链接
+        </button>
       </div>
     </footer>
 
@@ -731,6 +738,7 @@ function handleLogoError(event) {
 }
 
 </style> 
+
 
 
 
